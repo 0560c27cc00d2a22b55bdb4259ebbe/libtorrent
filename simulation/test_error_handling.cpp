5cbf17c56061cb73744bce6ff6293dbd734854ef
arvidn <arvid@cs.umu.se>
2017-12-29 02:39:23 +0100

    fix

--- simulation/test_error_handling.cpp
+++ simulation/test_error_handling.cpp
@@ -176,7 +176,7 @@ TORRENT_TEST(error_handling)
 		std::printf("\n\n === ROUND %d ===\n\n", i);
 		try
 		{
-			g_alloc_counter = 10 + i;
+			g_alloc_counter = 50 + i;
 			using namespace lt;
 			run_test(
 				[](lt::session&, lt::alert const*) {},
